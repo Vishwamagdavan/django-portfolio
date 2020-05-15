afd3e6e5314db883175094d0e3f9ec52c8ea2170
Vishwamagdavan <vishwamagadavan@gmail.com>
2020-05-15 09:38:25 +0530

    Quotes added

--- mpportfolio/home/views.py
+++ mpportfolio/home/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 from home.models import Works
+
+import requests
+
+
+url = 'https://quotes.rest/qod?category=management'
+api_token = "X-TheySaidSo-Api-Secret"
+headers = {'content-type': 'application/json',
+	   'X-TheySaidSo-Api-Secret': format(api_token)}
+
+response = requests.get(url, headers=headers)
+#print(response)
+quotes=response.json()['contents']['quotes'][0]
+print(quotes['quote'])
+
 # Create your views here.
 def works(request):
     works_list=Works.objects.all()
     context={
-        'works':works_list
+        'works':works_list,
+        'quote':quotes['quote']
     }
     return render(request,'home/index.html',context)
 def work_details(request,pk):
